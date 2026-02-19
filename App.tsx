4b2548146247be70fb61ba49b49f3312097a19cc
Ayush Gupta <aagm0404@gmail.com>
2026-02-19 22:04:20 +0530

    added app logo and splash screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import { RootNavigator } from './src/navigation';
 import { COLORS } from './src/theme';
+import RNBootSplash from "react-native-bootsplash";
+
 
 function App() {
+   useEffect(() => {
+  RNBootSplash.hide({ fade: true });
+}, []);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
--- android/app/src/main/java/com/fitzingapp/MainActivity.kt
+++ android/app/src/main/java/com/fitzingapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.fitzingapp
+import android.os.Bundle
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.zoontek.rnbootsplash.RNBootSplash
+
 
 class MainActivity : ReactActivity() {
 
@@ -12,7 +15,13 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "FitZingApp"
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
+
+    // ✅ BootSplash (must be AFTER super.onCreate)
+    RNBootSplash.init(this, R.style.BootTheme)
+  }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
